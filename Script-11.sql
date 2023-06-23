ad8ad89f6cccaafbec2e5b3be8d0dde4d79c7cb9
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-06-23 12:56:17 -0400

    Add: creacion bd
    Add: tabla usuario y tipo_usuario

--- Script-11.sql
+++ Script-11.sql
@@ -1,6 +1,22 @@
 CREATE database sprint_m5;
 use sprint_m5;
 
+CREATE TABLE Tipo_Usuario(
+	id_tipo_usuario int NOT NULL AUTO_INCREMENT,
+	nombre varchar(15),
+	PRIMARY KEY(id_tipo_usuario)	
+)
+
+
 CREATE TABLE Usuario(
-id_usuario
+	id_usuario int NOT NULL AUTO_INCREMENT,
+	nombre varchar(100),
+	apellido1 varchar(100),
+	apellido2 varchar(100),
+	fechar_nacimiento date,
+	run int,
+	contrasenia varchar(10),
+	tipo_usuario int,
+	PRIMARY KEY (id_usuario),
+	FOREIGN KEY (tipo_usuario) REFERENCES  Tipo_Usuario(id_tipo_usuario)
 )
